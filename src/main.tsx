1197fb9dbae18e925858a4e91182f355ebd98a61
Awesore <vadim.slisarenko.03@gmail.com>
2025-02-26 22:15:04 +0200

    Настройка для деплоя на GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 // src/index.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.ts";
 import { PersistGate } from "redux-persist/integration/react";
@@ -13,9 +13,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/site_recipe">
+        <HashRouter>
           <RoutesComponent />
-        </BrowserRouter>
+        </HashRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
